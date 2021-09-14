ad76984469e434371b030f5bae45670055e03d04
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-09-15 01:49:44 +0300

    Updated pom or Procfile

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,6 +1,7 @@
 package com;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        String port = System.getenv("PORT");
+        System.out.println("<<<<<<<<<<<<<<<<<<<MY PORT: "+port+" >>>>>>>>>>>>>>>>>>>");
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
         SpringApplication.run(Application.class, args);
     }
 
