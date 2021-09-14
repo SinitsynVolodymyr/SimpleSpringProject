f1f09dcb6dfd5583786dafa3ccbe485395bf4649
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-09-15 02:21:55 +0300

    Worked on Heroku!!!

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -10,18 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        /*
-        SpringApplication app = new SpringApplication(Application.class);
-        String port = System.getenv("PORT");
-        System.out.println("<<<<<<<<<<<<<<<<<<<MY PORT: "+port+" >>>>>>>>>>>>>>>>>>>");
-        System.out.println("args:");
-        for (String arg : args) {
-            System.out.println(arg);
-        }
-
-        app.setDefaultProperties(Collections
-                .singletonMap("server.port", port));
-        app.run(args);*/
         SpringApplication.run(Application.class, args);
     }
 
